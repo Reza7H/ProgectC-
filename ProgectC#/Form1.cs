53efd26eb323cc1deebe59235210502a5b18fbc4
KD <KD@DESKTOP-RN6Q98H>
2025-05-17 06:05:31 -0700

    Add project files.

---
+++ ProgectC#/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProgectC_
+{
+    public partial class Form1 : Form
+    {
+        List<double> numbers = new List<double>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double value;
+            if(double.TryParse(textBox1.Text, out value))
+            {
+                numbers.Add(value);
+               listBox1.Items.Add(value);
+               textBox1.Clear();
+                textBox1.Focus(); 
+            }
+            else
+            {
+                MessageBox.Show("Enter Correct Price");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Shopping TimeLine:"+"\n" +DateTime.Now.ToString());
+        }
+    }
+}
